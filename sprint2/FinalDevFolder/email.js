724c8184cac4411d71a53abaf824aca45037e922
Yair Lopez <ylo18@wirelessprv-10-195-43-15.near.illinois.edu>
2026-04-10 19:08:41 -0500

    push email

--- sprint2/FinalDevFolder/email.js
+++ sprint2/FinalDevFolder/email.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for port 465
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
